86fa774442755375b1f866b7eb595178f4c066f2
Cindyakaxinyi <54864780+Cindyakaxinyi@users.noreply.github.com>
2020-02-10 08:49:18 -0600

    Add files via upload

---
+++ Li_Cindy_2-8.py
@@ -0,0 +1,11 @@
+# Course: CS 30
+# Period: 1
+# Date created: 20/02/06
+# Date last modified: 20/02/10
+# Name: Cindy Li
+# Description: Exercise 2-8, different ways to print "8"
+
+print(4*2)
+print(5+3)
+print(1*8)
+print(6+2)
